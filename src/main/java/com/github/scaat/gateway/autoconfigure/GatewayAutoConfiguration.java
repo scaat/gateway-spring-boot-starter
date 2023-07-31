d0e554ae674c72e75b9b05903cea06fccd233143
Scaat Feng <scaat.feng@gmail.com>
2023-07-31 18:20:52 +0800

    [add]dynamic log adjustment

--- src/main/java/com/github/scaat/gateway/autoconfigure/GatewayAutoConfiguration.java
+++ src/main/java/com/github/scaat/gateway/autoconfigure/GatewayAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.github.scaat.gateway.autoconfigure;
 
 import com.github.scaat.gateway.config.GatewayPropertiesRefresher;
+import com.github.scaat.gateway.config.LoggerLevelRefresher;
 import com.github.scaat.gateway.filter.MyRoutePredicateFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
